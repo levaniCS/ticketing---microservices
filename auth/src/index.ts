41692ad9a9468209d095282fea0997cd6be946a8
levaniCS <levanisarishvili3105@gmail.com>
2021-06-13 23:40:03 +0400

    Fix: Ingress config and add jwt& cookie setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 require('express-async-errors');
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
@@ -12,14 +13,21 @@ import { NotFoundError } from './errors/not-found-error';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+)
 
 app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signUpRouter);
 app.use(signOutRouter);
 
-app.all('*', async () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 app.use(errorHandler);
