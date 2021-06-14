1afa791e62df8ef1b0ae8ccc11b05ebc2e6ba5c1
levaniCS <levanisarishvili3105@gmail.com>
2021-06-15 00:08:07 +0400

    Feat: add auth logic

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+// Inside of express app find request typee
+// definition and add additional property to it
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload
+        }
+    }
+}
+
+export const currentUser = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if(!req.session?.jwt) {
+        return next()
+      }
+    
+      try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+        req.currentUser = payload
+
+      } catch (err) {}
+
+    next()
+} 
