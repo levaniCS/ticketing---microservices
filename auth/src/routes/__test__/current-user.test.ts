dd27bc77a606789580db492e8189bf689010cd95
levaniCS <levanisarishvili3105@gmail.com>
2021-07-18 21:05:17 +0400

    ada

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -6,7 +6,7 @@ it('Responds with details about the current user', async () => {
 	const response = await request(app)
 		.get('/api/users/currentuser')
 		.set('Cookie', cookie)
-		.expect(200)
+		.expect(400)
 
 	expect(response.body.currentUser).toBeDefined()
 })
